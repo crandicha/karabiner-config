c7513e57c34b67395220033a6815fb66a93a54a6
Charles Randicha <charles.randicha@gmail.com>
2024-04-08 04:09:27 +0700

    fix hyper

--- rules.ts
+++ rules.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { KarabinerRules } from "./types";
-import { createHyperSubLayers, app, open } from "./utils";
+import { createHyperSubLayers, app, open, yabai } from "./utils";
 
 const rules: KarabinerRules[] = [
   // Define the Hyper key itself
@@ -21,6 +21,10 @@ const rules: KarabinerRules[] = [
               name: "hyper",
               value: 1,
             },
+          },
+          {
+            key_code: "left_shift",
+            modifiers: ["left_command", "left_control", "left_option"],
           },
         ],
         to_after_key_up: [
@@ -44,7 +48,9 @@ const rules: KarabinerRules[] = [
     // b = "B"rowse
     b: {
       g: open("https://github.com"),
-      j: open("https://borobudur.atlassian.net/jira/software/c/projects/PRCNG/boards/632?assignee=712020%3A73a7c48f-f6dc-40a1-abc7-82dd1cea1af3"),
+      j: open(
+        "https://borobudur.atlassian.net/jira/software/c/projects/PRCNG/boards/632?assignee=712020%3A73a7c48f-f6dc-40a1-abc7-82dd1cea1af3"
+      ),
       c: open("https://calendar.google.com"),
       m: open("https://mail.google.com"),
     },
@@ -54,7 +60,7 @@ const rules: KarabinerRules[] = [
       v: app("Visual Studio Code"),
       s: app("Slack"),
       t: app("Todoist"),
-      i: app('iTerm'),
+      i: app("iTerm"),
       0: app("Obsidian"),
       f: app("Finder"),
       p: app("Spotify"),
@@ -72,30 +78,6 @@ const rules: KarabinerRules[] = [
         ],
       },
     },
-
-    // r = "Raycast"
-    // r: {
-    //   n: open("raycast://script-commands/dismiss-notifications"),
-    //   l: open(
-    //     "raycast://extensions/stellate/mxstbr-commands/create-mxs-is-shortlink"
-    //   ),
-    //   e: open(
-    //     "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
-    //   ),
-    //   c: open("raycast://extensions/raycast/system/open-camera"),
-    //   p: open("raycast://extensions/raycast/raycast/confetti"),
-    //   a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
-    //   s: open("raycast://extensions/peduarte/silent-mention/index"),
-    //   h: open(
-    //     "raycast://extensions/raycast/clipboard-history/clipboard-history"
-    //   ),
-    //   1: open(
-    //     "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
-    //   ),
-    //   2: open(
-    //     "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
-    //   ),
-    // },
   }),
 ];
 
